898a0b952fe5afb7a78d4b8b6169de956aaf8ba7
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-08-22 13:42:51 -0600

    Added data table dependencies.

--- src/components/AllHoldingsSummaryTable.tsx
+++ src/components/AllHoldingsSummaryTable.tsx
@@ -17,6 +17,9 @@ export function AllHoldingsSummaryTable({
   return (
     <div className="flex flex-col">
       <div className="text-center py-4 border-b border-solid">Top 5 Holdings by Value</div>
+      <Link href={`performance/${fund}/all-holdings`}>
+        <div className="">View All</div>
+      </Link>
       {allHoldingsSummary.holdings
       .filter((a) => a.active)
       .slice(0, 5)
