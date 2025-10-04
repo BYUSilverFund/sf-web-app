5848621e9420930a658468030ddd065f75ca9b27
Ben Haggard <21bhaggard@gmail.com>
2025-10-03 19:00:59 -0600

    update metadata and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,33 @@ const robotoMono = Roboto_Mono({
 export const metadata: Metadata = {
   title: "BYU Silver Fund",
   description: "BYU's Premier Student-Run Investment Fund",
+  keywords: [
+    "BYU",
+    "Silver Fund",
+    "Student Investment Fund",
+    "Finance",
+    "Investing",
+    "Student-Run Fund",
+    "Brigham Young University",
+  ],
+  authors: [{ name: "BYU Silver Fund", url: "https://silverfund.byu.edu" }],
+  creator: "BYU Silver Fund",
+  openGraph: {
+    title: "BYU Silver Fund",
+    description: "BYU's Premier Student-Run Investment Fund",
+    url: "https://silverfund.byu.edu",
+    siteName: "BYU Silver Fund",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BYU Silver Fund",
+    description: "BYU's Premier Student-Run Investment Fund",
+  },
+  icons: {
+    icon: "/sf-logo-white.png",
+  },
 };
 
 // Define the root layout component for the application
