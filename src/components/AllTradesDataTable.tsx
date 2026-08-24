62335f3b090e5d92822c7b5510df1b56a3aaa80b
Ben Haggard <21bhaggard@gmail.com>
2026-08-24 13:05:30 -0600

    no data message consistency in data tables

--- src/components/AllTradesDataTable.tsx
+++ src/components/AllTradesDataTable.tsx
@@ -326,8 +326,10 @@ export function AllTradesDataTable({
                 <TableRow className="h-[33.33vh]">
                   <TableCell
                     colSpan={table.getAllLeafColumns().length}
-                    className="text-center"
-                  ></TableCell>
+                    className="text-center text-sm text-gray-500"
+                  >
+                    No trades found.
+                  </TableCell>
                 </TableRow>
               )}
             </TableBody>
